997129c18d7ba629b183655e9995fe99e7b2b670
EDU-DOMAIN\qriehle <qriehle@mail.pima.edu>
2017-09-28 19:58:07 -0700

    no message

---
+++ duet_cody-quintin/Assets/movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+    public float speed = 1f;
+    public float rotationspeed = 1f;
+    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        transform.Rotate(0f,Input.GetAxis("Horizontal")*Time.deltaTime* rotationspeed,0f);
+        transform.Translate(0f,0f,Input.GetAxis("Vertical") * Time.deltaTime* speed);
+    }
+}
