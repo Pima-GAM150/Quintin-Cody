c17cd8515d7ef0629f6ab61a874ebaf4a65a8cb8
ASUS-WOODS\Cody <woodscody4@gmail.com>
2017-10-10 21:55:59 -0700

    Github test--- making track on terrain in unity

---
+++ duet_cody-quintin/Assets/LapCompleteTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LapCompleteTrigger : MonoBehaviour {
+
+    public GameObject LapCompleteTrig;
+
+    void OnTriggerEnter()
+    {
+        LapCompleteTrig.SetActive(true);
+    }
+}
