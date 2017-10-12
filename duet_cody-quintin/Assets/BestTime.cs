f74dc050fc4e572b118273e56e381a0f15e56830
DESKTOP-8JCA1RT\Quintin <qriehle@mail.pima.edu>
2017-10-12 00:04:42 -0700

    scripts and finishline object

---
+++ duet_cody-quintin/Assets/BestTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class BestTime : MonoBehaviour {
+    private float score = 0.0f;
+    public Text BestText;
+    // Use this for initialization
+    void Start () {
+        BestText.text = "Best : " + ((int)PlayerPrefs.GetFloat("Best")).ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(PlayerPrefs.GetFloat("Best") < score)
+        PlayerPrefs.SetFloat("Best", score);
+        
+
+    }
+}
---
+++ duet_cody-quintin/Assets/FinishLine.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour {
+
+    public GameObject Finish;
+    private void OnTriggerEnter;
+    
+    
+    
+    
+   
+}
